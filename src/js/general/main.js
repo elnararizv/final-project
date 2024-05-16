b67ee35e7039a8246cac195b40e282875f34c8b7
elnara002 <6fdtxsg@code.edu.az>
2024-05-16 21:43:00 +0400

    added generate id function

--- src/js/general/main.js
+++ src/js/general/main.js
@@ -97,3 +97,12 @@ function checkSign() {
     // profileName.textContent=fnameInput.value
   }
 }
+
+function generate_uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+      function (c) {
+          const uuid = Math.random() * 16 | 0;
+          const v = c == 'x' ? uuid : (uuid & 0x3 | 0x8);
+          return v.toString(16);
+      });
+}
