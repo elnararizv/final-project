625399908ade565087a0ec75522c7005cd55b6d2
aygunqasimova <aygun.qasimova546@gmail.com>
2024-05-17 11:57:22 +0400

    book-page-js-fix

---
+++ src/js/book.js
@@ -0,0 +1,69 @@
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+        import { getDatabase, set, ref, get } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyBU0sULZYB6-mZdPwFsWw-teB5STW0xBoc",
+            authDomain: "bookstore-44df6.firebaseapp.com",
+            databaseURL: "https://bookstore-44df6-default-rtdb.firebaseio.com",
+            projectId: "bookstore-44df6",
+            storageBucket: "bookstore-44df6.appspot.com",
+            messagingSenderId: "520272877168",
+            appId: "1:520272877168:web:0245c151c6b0c41c62c6b5",
+            measurementId: "G-WKRXPCNFEL"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+        const db = getDatabase();
+
+        const commentInp = document.querySelector('#comment-inp')
+        const send = document.querySelector('#send-btn')
+        const bookText = document.querySelector('.bookText')
+        function generate_uuidv4() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+                function(c) {
+                    const uuid = Math.random() * 16 | 0;
+                    const v = c == 'x' ? uuid : (uuid & 0x3 | 0x8);
+                    return v.toString(16);
+                });
+        }
+        function CommentSetData() {
+            const id = generate_uuidv4();
+            set(ref(db, 'Comment/' + id), {
+                Comment: commentInp.value,
+                Id: id,
+            })
+            .then(() => {
+                alert('Data Push');
+                CommentGetData();
+            })
+            .catch((err) => errorAlert("Error"));
+        }
+        function CommentGetData() {
+            get(ref(db, 'Comment'))
+            .then(data => {
+                bookText.innerHTML = "";
+                data.forEach(childSnapshot => {
+                    const comment = childSnapshot.val().Comment;
+                    const sender = "anonim";
+                    const time = new Date();
+                    const day = time.getDate();
+                    const hours = time.getHours();
+                    const minutes = time.getMinutes();
+                    const formattedTime = `${day} ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+                    const commentDiv = document.createElement("div");
+                    commentDiv.innerHTML = `<strong>${sender}:</strong> ${comment}<br><br> ${formattedTime}`;
+                    bookText.appendChild(commentDiv);
+                });
+            })
+            .catch(err => { alert(err) });
+        }
+        send.addEventListener('click', CommentSetData);
