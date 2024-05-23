3b41c5226d8ae5aeb472e4a2c90d395f84b9d79e
Zibeydee <zibeydeceferli2012@gmail.com>
2024-05-23 14:03:20 +0400

    updated code

--- src/js/book.js
+++ src/js/book.js
@@ -6,9 +6,7 @@ import {
   set,
   ref,
   get,
-  update,
-  push,
-  child,
+  onValue
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -35,90 +33,153 @@ const db = getDatabase();
 const commentInp = document.querySelector("#comment-inp");
 const send = document.querySelector("#send-button");
 const bookText = document.querySelector(".bookText");
-function generate_uuidv4() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const uuid = (Math.random() * 16) | 0;
-    const v = c == "x" ? uuid : (uuid & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
+
+ const bookId = window.location.search.substring(1);
+  const currentTime = new Date(); 
+  const options = { timeZone: 'Asia/Baku' }; 
+  const timestamp = currentTime.toLocaleString('en-US', options); 
+  console.log(timestamp)
 
 function CommentSetData() {
   const id = generate_uuidv4();
-
-  set(ref(db, "books/Comment/" + id), {
+ 
+  set(ref(db, "books/" + bookId + "/comments/" + id), {
     Comment: commentInp.value,
-    Id: id,
+    time: timestamp, 
+    Id: id
   })
-    .then(() => {
-      // alert('Data Push');
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 1000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        },
-      });
-      Toast.fire({
-        icon: "success",
-        title: "Signed in successfully",
-      });
-      commentInp.value = "";
-      commentInp.placeholder = "Your anonim comment";
-    })
-    .catch((err) => errorAlert("Error"));
+  .then(() => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Your anonim comment is sent",
+    });
+    commentInp.value ="";
+   
+  })
+  .catch(() =>{ const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+  Toast.fire({
+    icon: "error",
+    title: "Comment couldn't be sent",
+  })})
 }
+
 send.addEventListener("click", CommentSetData);
 
-const time = new Date();
-const day = time.getDate();
-const hours = time.getHours();
-const minutes = time.getMinutes();
-const formattedTime = `${day} ${hours}:${
-  minutes < 10 ? "0" + minutes : minutes
-}`;
-
 function CommentGetData() {
-  get(ref(db, "books/Comment/")).then((data) => {
-    const comments = data.val();
-    console.log(comments);
-    Object.values(comments).forEach((el) => {
-      console.log(el);
-      bookText.innerHTML += `<li class="book-container">
-                    <span class="fw-bold">anonim</span> 
-                    <span>${formattedTime}</span>
-                    <p>${el.Comment}</p>
-                    </li>`;
-    });
+  const bookId = window.location.search.substring(1);
+  onValue(ref(db, "books/" + bookId + "/comments/"), (snapshot) => {
+    const comments = snapshot.val();
+    if (comments) {
+      Object.values(comments).forEach((el) => {
+        const commentDate = new Date(el.time);
+        const currentDate = new Date();
+        const oneDay = 24 * 60 * 60 * 1000;
+        const timeDiff = Math.abs(currentDate - commentDate);
+        const daysDiff = Math.round(timeDiff / oneDay);
+
+        let timeString = "";
+        if (daysDiff === 0) {
+          timeString = "Today " + commentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (daysDiff === 1) {
+          timeString = "Yesterday " + commentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (daysDiff <= 7) {
+          timeString = daysDiff + " day(s) ago";
+        } else {
+          timeString = "Added " + daysDiff / 7 + " week(s) ago";
+        }
+
+        bookText.innerHTML += `<li class="book-container">
+          <span class="fw-bold">anonim</span>
+          <span>${timeString}</span>
+          <p>${el.Comment}</p>
+          </li>`;
+      });
+    } else {
+      console.log('ERROR')
+    }
   });
 }
+
 
 CommentGetData();
+const bookInfo = document.querySelector(".book-info");
 
-const bookId = window.location.search.substring(1);
-const bookInfo = document.querySelector("#section1");
+BookGetData();
+
 function BookGetData() {
-  get(ref(db, "books/" + bookId)).then((data) => {
-    bookInfo.innerHTML = `
-                <div class="buttons">
-        <button class="back">< BACK</button>
-      </div>
-      <div class="book">
-        <div class="div1">
-          <h4 class="year">${data.val().date}</h4>
-          <h6 class="order">${data.val().name}</h6>
-          <h5 class="day">Gelen vaxt</h5>
-          <h1 class="text-title">${data.val().author}</h1>
-          <p class="text">${data.val().description}</p>
+  onValue(ref(db, "books/" + bookId), (snapshot) => {
+    const data = snapshot.val();
+    const bookInfo = document.querySelector(".book-info");
+
+    if (data) {
+      const currentDate = new Date();
+      const addedDate = new Date(data.addedAt);
+      const oneDay = 24 * 60 * 60 * 1000;
+      const timeDiff = Math.abs(currentDate - addedDate);
+      const daysDiff = Math.floor(timeDiff / oneDay);
+
+      let timeString = "";
+      if (daysDiff === 0) {
+        timeString = "Today";
+      } else if (daysDiff === 1) {
+        timeString = "Yesterday";
+      } else if (daysDiff <= 7) {
+        timeString = daysDiff + " day(s) ago";
+      } else {
+        timeString = addedDate.toLocaleDateString();
+      }
+
+      bookInfo.innerHTML = `
+        <div class="book-details">
+          <span class="year">${data.date.substring(0,4)}</span>
+          <h1 class="bookName">${data.name}</h1>
+          <h4 class="day">${timeString}</h4>
+          <h2 class="book-author">${data.author}</h2>
+          <p class="description">${data.description}</p>
+          <span class="moreDetails">More details</span>
         </div>
-        <div class="div2">
-          <img class = 'bokImg' src="${data.val().image}"/>
+        <div class="book-img">
+          <img src="${data.image}" alt="">
         </div>
-                `;
+      `;
+      
+      const description = document.querySelector(".description");
+      const moreDetails = document.querySelector(".moreDetails");
+      const shortText = description.textContent.substring(0, 560);
+      const fullText = description.textContent;
+
+      let isLong = true;
+      moreDetails.addEventListener("click", function() {
+        if (isLong) {
+          description.textContent = fullText;
+          moreDetails.textContent = "Hide Details";
+        } else {
+          description.textContent = shortText;
+          moreDetails.textContent = "More Details";
+        }
+        isLong = !isLong;
+      });
+    }
   });
 }
-BookGetData();
